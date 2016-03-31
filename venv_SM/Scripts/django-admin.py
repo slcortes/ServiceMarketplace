ec767c3bd0cef493e3f7c7ea8aa6c9a14a8dabd5
Anthony Lackey <alackey96@gmail.com>
2016-03-31 16:41:01 -0700

    remove venv_SM

--- venv_SM/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\jerome\documents\servicemarketplace\venv_sm\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
