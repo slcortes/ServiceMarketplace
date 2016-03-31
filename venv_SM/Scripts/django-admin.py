9b4fb72cb012f928120f08d9d07bbc4976cf9519
Jerome Francisco <jeromefrancisco21@yahoo.com>
2016-03-31 16:30:17 -0700

    added homepage and base

---
+++ venv_SM/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jerome\documents\servicemarketplace\venv_sm\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
