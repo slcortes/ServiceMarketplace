2e43a44b9557285fbdc937ba85296564594c8001
nliupeng <nliupeng@gmail.com>
2016-04-08 13:34:13 -0700

    front end update: my_account

--- market/admin.py
+++ market/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Service, Review
+from .models import Service, Review, Bid
 
 
 admin.site.register(Service)
 admin.site.register(Review)
+admin.site.register(Bid)
